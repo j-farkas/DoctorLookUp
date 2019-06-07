585827281656498dc43471d4bece8087408aeab0
Student <example@example.com>
2019-06-07 08:49:44 -0700

    add ability to take input

---
+++ src/doctor.js
@@ -0,0 +1,21 @@
+export class DoctorLookUp {
+    constructor(){
+      this.conditions = {};
+    }
+  getConditions(ailment) {
+    return new Promise(function(resolve, reject) {
+
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}&query=${ailment}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-//require('exports-loader?file!./bootstrap/js/dist/.js')
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
-//$(document).ready(function() {
+import { DoctorLookUp} from './doctor';
+import $ from 'jquery';
 
-//});
+
+$("#query").on("submit", function(event){
+  let ailment = $(".ailment").text();
+   let promise = doctor.getConditions(ailment);
+
+   promise.then(function(response) {
+     let body = JSON.parse(response);
+     console.log(body);
+     //doctor.conditions = body;
+//        $("#Ailment").append(`<option value = "${body.data.uid}">${body.data.name}</option>`);
+   }, function(error) {
+     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+   });
+   event.preventDefault();
+});
+let doctor = new DoctorLookUp();
+$(document).ready(function() {
+  // $('#weatherLocation').click(function() {
+  //     let city = $('#location').val();
+  //     $('#location').val("");
+
+  });
