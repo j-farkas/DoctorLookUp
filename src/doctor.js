d6f7b1074ad5bc4b901543292c0f188a41208349
Student <example@example.com>
2019-06-07 08:52:37 -0700

    Change location searched to Portland

--- src/doctor.js
+++ src/doctor.js
@@ -6,7 +6,7 @@ export class DoctorLookUp {
     return new Promise(function(resolve, reject) {
 
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}&query=${ailment}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}&query=${ailment}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
