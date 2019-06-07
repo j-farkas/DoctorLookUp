041741d32bd8a75003eb536225bcd498aa435938
Student <example@example.com>
2019-06-07 16:35:05 -0700

    Cleanup

--- src/doctor.js
+++ src/doctor.js
@@ -1,7 +1,5 @@
 export class DoctorLookUp {
   getDoctors(key, search) {
-    console.log(key);
-    console.log(search);
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.520%2C-122.677&skip=0&limit=10&user_key=${process.env.exports.apiKey}&${key}=${search}`;
